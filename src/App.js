6b526a7af78dd5af36480c0d00bd743d582850b6
Faiz Hameed <faizftb@gmail.com>
2019-06-19 21:12:48 +0530

    New Responsive web app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+componentDidMount (){
+  const prevScrollpos = 0;
+
+  window.onscroll = function() {
+    var element = document.getElementById("nav");
+    if(prevScrollpos!==window.pageYOffset)
+  {
+  element.classList.add("visual")}
+  else
+  element.classList.remove("visual");
+}
+
+}
+
+  render(){
+
+
+    return (
+      <div className="App">
+        <div>
+        <img   className ="imgcont" src="https://images.unsplash.com/photo-1559163179-3fd017552229?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Italian Trulli"></img>
+        </div>
+        <div id ="nav" className="nav-container">
+       <ul>
+        <li className="navitem">Home</li>
+        <li className="navitem">Blog</li>
+        <li className="navitem">Projects</li>
+        <li className="navitem">About</li>
+        <li className="navitem">Contact</li>  
+       </ul>
+        </div>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
