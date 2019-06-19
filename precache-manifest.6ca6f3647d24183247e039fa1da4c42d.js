4c619f2f240d5c946d4d7bb6a661bf66d68702d7
Faiz Hameed <faizftb@gmail.com>
2019-06-19 21:14:25 +0530

    Updates

---
+++ precache-manifest.6ca6f3647d24183247e039fa1da4c42d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c727d4e8e3169ec646f0c9393fa2b7",
+    "url": "/responsiveweb/index.html"
+  },
+  {
+    "revision": "5ff20f0e6780609616a5",
+    "url": "/responsiveweb/static/css/main.cd0bd524.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsiveweb/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5ff20f0e6780609616a5",
+    "url": "/responsiveweb/static/js/main.9684687e.chunk.js"
+  },
+  {
+    "revision": "f01efd7c80a2d1a5ce23",
+    "url": "/responsiveweb/static/js/runtime~main.97dc7397.js"
+  }
+]);
